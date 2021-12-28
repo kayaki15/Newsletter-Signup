06cd5dd9f735ace9196a6fc3fa168bb5b6370d6b
soulaiman <61231576+kayaki15@users.noreply.github.com>
2021-12-28 16:17:05 +0100

    improvments

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const request = require('request');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static("public") ) ;
+
+app.use(bodyParser.urlencoded({extended : true }));
+{}
+
+app.get("/" , function(req , res){
+res.sendFile(__dirname + "/signup.html");
 })
+
+app.post("/" , function(req , res){
+
+///////////////////::this part for showing in console :
+
+console.log(req.body);
+var fName = req.body.firstName;
+var lName = req.body.lastName;
+var email = req.body.email;
+////////////////////////////// this part to display data in Browser :
+
+var result = "<h4>First Name</h4>" + fName + "<h4>Last Name</h4> " +  lName  + "<h4>E-mail </h4> "   + email;
+  res.send("The Data is  : " + result);
+
+});
+
 
 app.listen(port, () => {
   console.log(`setup is running on 3000 http://localhost:${port}`)
