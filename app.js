58d20c8ec86b4d6e9350f24442fcae5de5acc52e
soulaiman <61231576+kayaki15@users.noreply.github.com>
2022-01-01 16:42:48 +0100

    improvments in app.js

--- app.js
+++ app.js
@@ -6,66 +6,72 @@ const https = require('https');
 const app = express()
 const port = 3000
 
-app.use(express.static("public") ) ;
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended : true }));
-{}
+app.use(bodyParser.urlencoded({
+  extended: true
+})); {}
 
-app.get("/" , function(req , res){
-res.sendFile(__dirname + "/signup.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/" , function(req , res){
+app.post("/", function(req, res) {
 
-///////////////////::this part for showing in console :
-
-
-const firstName = req.body.fName;
-const lastName = req.body.lName;
-const email = req.body.email;
-// console.log(req.body);
-
-////////////////////////////// this part to display data in Browser :
- const data = {
-   members : [
-     {
-       email_address: email ,
-      status:"subscribed",
-    merge_fields:{
-         FNAME :firstName ,
-         LNAME :lastName
-
-       }
-
-          }
-            ]
-          } ;
-
-//
-// var result = "<h4>First Name</h4>" + fName + "<h4>Last Name</h4> " +  lName  + "<h4>E-mail </h4> "   + email;
-//   res.send("The Data is  : " + result);
-const jsonData = JSON.stringify(data) ;
-const url = 'https://us20.api.mailchimp.com/3.0/lists/f5d8787336';
+  ///////////////////::this part for showing in console :
 
 
-const options = {
-  method :"POST",
-  auth : "benomar:04238645c2d9676094ef765c9e7f7243-us20"
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  // console.log(req.body);
 
-}
-const request = https.request(url , options , function(response){
-  response.on("data" , function(data){
-  console.log(JSON.parse(data));
+  ////////////////////////////// this part to display data in Browser :
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
 
-})
+      }
 
-})
+    }]
+  };
 
-request.write(jsonData) ;
-request.end();
+  //
+  // var result = "<h4>First Name</h4>" + fName + "<h4>Last Name</h4> " +  lName  + "<h4>E-mail </h4> "   + email;
+  //   res.send("The Data is  : " + result);
+  const jsonData = JSON.stringify(data);
+  const url = 'https://us20.api.mailchimp.com/3.0/lists/f5d8787336';
 
 
- });
+  const options = {
+    method: "POST",
+    auth: "benomar:04238645c2d9676094ef765c9e7f7243-us20"
+
+  }
+  const request = https.request(url, options, function(response) {
+
+      if (response.statusCode === 200) {
+        res.send("Successufully subscribed") ;
+      } else {
+        res.send("There was an error with signing up , please ty  again!");
+      }
+
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+
+    })
+
+  })
+
+request.write(jsonData); request.end();
+
+
+});
 
 
 app.listen(port, () => {
